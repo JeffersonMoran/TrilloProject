13e006b550930951369bbb1bf480d18049ee514d
JeffersonMoran <jeffersonmoran79@gmail.com>
2019-03-04 02:56:07 -0300

    finishing the header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import Navigation from './components/Navigation';
-import Hotel from './layouts/Hotel';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Hotel from "./layouts/Hotel";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Header />
-        <div className="content">
-          <Navigation />
-          <Hotel />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container">
+                <Header />
+                <div className="content">
+                    <Navigation />
+                    <Hotel />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import glassIcon from "../img/SVG/magnifying-glass.svg";
 
 const SearchBar = () => (
     <form action="#" className="search">
         <input type="text" className="search__input" placeholder="Procure Hotéis" />
         <button className="search__button">
-            <img src={require("../img/SVG/magnifying-glass.svg")} className="search__icon" alt="search" />
+            <img src={glassIcon} className="search__icon" alt="search" />
         </button>
     </form>
 )
