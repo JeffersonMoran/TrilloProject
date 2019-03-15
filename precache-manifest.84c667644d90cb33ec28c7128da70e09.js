45f91e119b5beb6693e694dc41b2be0237df53a6
JeffersonMoran <jeffersonmoran79@gmail.com>
2019-03-15 02:36:34 -0300

    Updates

---
+++ precache-manifest.84c667644d90cb33ec28c7128da70e09.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "84aa053b49b1cb0d0c65783f5241c271",
+    "url": "/TrilloProject/static/media/aircraft-take-off.84aa053b.svg"
+  },
+  {
+    "revision": "f4e35d625d146418b3bb",
+    "url": "/TrilloProject/static/css/main.0d85db6a.chunk.css"
+  },
+  {
+    "revision": "69d22c7fd3f5d3fbcd03",
+    "url": "/TrilloProject/static/js/runtime~main.69d22c7f.js"
+  },
+  {
+    "revision": "522f840fee064a4110ff",
+    "url": "/TrilloProject/static/js/2.522f840f.chunk.js"
+  },
+  {
+    "revision": "09bf8ae3ac2fcd070e04781738e4bea0",
+    "url": "/TrilloProject/static/media/magnifying-glass.09bf8ae3.svg"
+  },
+  {
+    "revision": "b1e358dac9f6ed93b77e3a26554601a6",
+    "url": "/TrilloProject/static/media/bookmark.b1e358da.svg"
+  },
+  {
+    "revision": "807994987f885922725b32722b8ec357",
+    "url": "/TrilloProject/static/media/chat.80799498.svg"
+  },
+  {
+    "revision": "f5fea7c4e927d81b1637b07af03a5714",
+    "url": "/TrilloProject/static/media/user.f5fea7c4.jpg"
+  },
+  {
+    "revision": "f4e35d625d146418b3bb",
+    "url": "/TrilloProject/static/js/main.f4e35d62.chunk.js"
+  },
+  {
+    "revision": "b5a4738667b39329dd542b07337aa67b",
+    "url": "/TrilloProject/static/media/chevron-right.b5a47386.svg"
+  },
+  {
+    "revision": "03f24c33dacb18eeaba38414ff6f2b94",
+    "url": "/TrilloProject/static/media/home.03f24c33.svg"
+  },
+  {
+    "revision": "ee4314ab0030690bbbddff6bcf9c2e11",
+    "url": "/TrilloProject/static/media/key.ee4314ab.svg"
+  },
+  {
+    "revision": "d6337acc65d95ea9c6b73635de5acec2",
+    "url": "/TrilloProject/static/media/location-pin.d6337acc.svg"
+  },
+  {
+    "revision": "d5a4f7e186267e097a12f5ee28aff571",
+    "url": "/TrilloProject/static/media/map.d5a4f7e1.svg"
+  },
+  {
+    "revision": "1f1e3e5583346a60ec12ffcc12486af8",
+    "url": "/TrilloProject/static/media/star.1f1e3e55.svg"
+  },
+  {
+    "revision": "4685d704c8c9aeb51971b5d808908b94",
+    "url": "/TrilloProject/index.html"
+  }
+];
