4057e7d6b4f8661c5d85f470a3a2749f9fed9321
JeffersonMoran <jeffersonmoran79@gmail.com>
2019-03-15 02:32:30 -0300

    Navbar finished

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import Navigation from "./components/Navigation";
 import Hotel from "./layouts/Hotel";
 
 class App extends Component {
+    state = {
+        navigationData : [
+            { name: 'Hotel', icon: 'home', alt: 'Hotel', active: true },
+            { name: 'Passagens', icon: 'aircraft-take-off', alt: 'Passagens',  active: false },
+            { name: 'Aluguéis', icon: 'key', alt: 'Aluguéis', active: false },
+            { name: 'Passeios', icon: 'map', alt: 'Passeios', active: false },
+        ]
+    }
+
+
     render() {
         return (
             <div className="container">
-                <Header />
+                <Header/>
                 <div className="content">
-                    <Navigation />
+                    <Navigation navigationData={this.state.navigationData} />
                     <Hotel />
                 </div>
             </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const Navigation = () => (
+const Navigation = ({ navigationData }) => (
     <nav className="sidebar">
-        Navigation
+        <ul className="side-nav">
+            {
+                navigationData.map(item => (
+                    <li className={`side-nav__item ${item.active ? 'side-nav__item--active':''}`}>
+                        <a href="#" className="side-nav__link">
+                            <img src={require(`../img/SVG/${item.icon}.svg`)} className="side-nav__icon" alt={item.alt} />
+                            <span>{item.name}</span>
+                        </a>
+                    </li>
+                ))
+            }
+        </ul>
+
+        <div className="legal">
+            &copy; 2019 por Idol.Todos os direitos reservados.
+        </div>
     </nav>
 )
 
